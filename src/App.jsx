227a114db9bcca83c0804bfc31c512e892c11b8a
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-12-24 12:16:12 +0500

    RTK query

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
+import { useCreatePostMutation, useGetPostsQuery } from './services/jsonPlaceholderApi';
 
 
 function App() {
-  const [newPost, setNewPost] = useState({ title: "", body: "", id: Date.now()});
+  const [newPost, setNewPost] = useState({ title: "", body: "", id: Date.now() });
+  const { data, error, isLoading, refetch } = useGetPostsQuery();
+  const [createPost, { isLoading: isCreating, error: createError }] =
+    useCreatePostMutation();
+  if (isLoading) return <p> Loading...</p>;
 
+  if (createError) return <p> There was an error creating a post</p>;
+
+  if (error) return <p> There was an error :</p>;
+  const handleCreatePost = async () => {
+    console.log(newPost);
+    
+    await createPost(newPost);
+    refetch()
+  }
   return (
     <>
-        <h1> Leave A Like : </h1>
+      <h1> Leave A Like : </h1>
+      <div>
+        <input type="text" placeholder="Title..." value={newPost.title} onChange={(e) => setNewPost(
+          (prev) => ({ ...prev, title: e.target.value })
+        )} />
+        <input type="text" placeholder="Body..." value={newPost.body} onChange={(e) => setNewPost(
+          (prev) => ({ ...prev, body: e.target.value })
+        )} />
+        <button onClick={handleCreatePost} disabled={isCreating} >Create post</button>
         <div>
-          <input type="text"  placeholder="Title..." />
-          <input type="text"  placeholder="Body..."/>
-          <button>Create post</button>
-          <div>
-
-          </div>
+          {data?.map((post) => (
+            <p key={post.id}> {post.title}</p>
+          ))}
         </div>
+      </div>
     </>
   )
 }
--- src/services/jsonPlaceholderApi.js
+++ src/services/jsonPlaceholderApi.js
@@ -4,16 +4,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const jsonPlaceholderApi = createApi({
     reducerPath: "jsonPlaceholderApi",
     baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
-    endpoints:(builder)=>({
-        getPosts:builder.query({query:()=>'posts'}),
-        createPost:builder.mutation({
-            query:(newPost)=>({
-                url:'posts',
-                method:"POST",
-                body: newPost,
-            })
+    endpoints: (builder) => ({
+        getPosts: builder.query({ query: () => 'posts' }),
+        createPost: builder.mutation({
+            query: (newPost) => {
+                console.log(newPost);
+                return {
+                    url: 'posts',
+                    method: 'POST',
+                    body: newPost,
+                };
+            }
         })
     })
 })
 
-export const {useGetPostsQuery,useCreatePostMutation}= jsonPlaceholderApi
+export const { useGetPostsQuery, useCreatePostMutation } = jsonPlaceholderApi
